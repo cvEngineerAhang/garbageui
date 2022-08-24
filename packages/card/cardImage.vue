ffd1237cdae49889f1100b9c407d67d2cb3c514a
Ahang <jiacheng_niii@outlook.com>
2022-08-24 19:22:18 +0800

    update packages

---
+++ packages/card/cardImage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="j-card-image">
+    <img class="img" :src="src" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "JCardImage",
+  props: {
+    src: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.j-card-image {
+  width: 100%;
+  height: 100%;
+  .img {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
--- packages/index.js
+++ packages/index.js
@@ -2,7 +2,9 @@ import Vue from "vue";
 import Button from "./button/button";
 import Dialog from "./dialog/dialog";
 import Input from "./input/input";
-const components = [Button, Dialog,Input];
+import Card from "./card/card";
+import CardImage from "./card/cardImage";
+const components = [Button, Dialog, Input, Card, CardImage];
 const install = function (vue) {
   components.forEach((item) => {
     Vue.component(item.name, item);
