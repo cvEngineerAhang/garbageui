2eb5da3062b80376382233c609b9ec11886811f1
Ahang <jiacheng_niii@outlook.com>
2022-08-27 20:46:42 +0800

    update card_2

--- packages/index.js
+++ packages/index.js
@@ -4,7 +4,19 @@ import Dialog from "./dialog/dialog";
 import Input from "./input/input";
 import Card from "./card/card";
 import CardImage from "./card/cardImage";
-const components = [Button, Dialog, Input, Card, CardImage];
+import CardListTitle from "./card/cardListTitle";
+import CardListItem from "./card/cardListItem";
+import CardListActions from "./card/cardListActions";
+const components = [
+  Button,
+  Dialog,
+  Input,
+  Card,
+  CardImage,
+  CardListTitle,
+  CardListItem,
+  CardListActions,
+];
 const install = function (vue) {
   components.forEach((item) => {
     Vue.component(item.name, item);
