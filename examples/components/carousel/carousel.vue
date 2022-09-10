e5ec790d255293457b99eda2d58fda4daf59c810
Ahang <jiacheng_niii@outlook.com>
2022-09-10 21:50:11 +0800

    9-10

--- examples/components/carousel/carousel.vue
+++ examples/components/carousel/carousel.vue
@@ -1,13 +1,27 @@
 <template>
-  <div class="carousel"></div>
+  <div class="j-carousel" :style="{ height }">
+    <slot></slot>
+  </div>
 </template>
 
 <script>
 export default {
   name: "j-carousel",
+  props: {
+    height: {
+      type: String,
+      default: "0",
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-
+.j-carousel {
+  display: flex;
+  width: 50%;
+  overflow: hidden;
+  margin:  0 auto;
+  border:1px solid red;
+}
 </style>
